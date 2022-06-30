8532ad1e8cfd513648b957d84ef271865a427c49
Jackie Collas <jackie@moreira.com>
2022-06-30 14:55:08 -0400

    quest edits

--- js/querent-quest.js
+++ js/querent-quest.js
@@ -14,7 +14,8 @@ const questElem = {
         "a really cool cloak",
         "information",
         "financial compensation",
-        "a really big knife"
+        "a really big knife",
+        "a potion"
     ],
     property: [
         "used to belong to a famous general",
@@ -26,14 +27,14 @@ const questElem = {
         "is a vital clue in an ongoing mystery or investigation",
         "has become corrupted",
         "hurts people",
-        "taps into a person's abmition and drive them to cruelty"
+        "taps into a person's ambition and drives them to cruelty"
     ],
     goal: [
         "so that we can settle a grudge",
         "because it will provide us with strength in the coming days",
         "because we are duty-bound to honor an agreement",
         "so that we can take down one of our enemies",
-        "to tip the scales in our favor and gain the upper hand",
+        "to tip the scales in our favor and gain the upper hand in an ongoing conflict",
         "so that we can kick someone's ass",
         "for Scooby Doo reasons",
         "as a means to depose someone in power",
@@ -42,14 +43,14 @@ const questElem = {
     ],
     warning: [
         "this is the perfect opportunity to take vengeance",
-        "the upcoming choices should never be made by one person alone",
-        "victory wil come at great cost and sacrifice",
+        "the upcoming choices should not be made by one person alone",
+        "victory will come at great cost and sacrifice",
         "we don't have all of the information",
         "there's a good chance that we will be attacked by a werewolf",
-        "we need to be careful not to piss anybody off otherwise we WILL be having an afternoon tea party with the Barristers",
-        "new and scary things aren't always so bad",
+        "we need to be careful not to piss anybody off otherwise we WILL be having an afternoon tea party with the Barristers (aka die horribly)",
+        "we should remember that new and scary things aren't always so bad",
         "we are liable for our own actions and have no one to blame for the consequences but ourselves",
-        "however bad we think this is going to be, it is going to be WAY worse. Like way worse. Oh my god",
+        "however bad we think this adventure is going to be, it is going to be WAY worse. Like way worse. Oh my god",
         "while we may accomplish our goals, there is a good chance that we will lose sight of what is most important in the process"
     ]
 }
